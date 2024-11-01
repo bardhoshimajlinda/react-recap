441b0343746831ba16e65191c828754c3eb2a7e3
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-11-01 21:29:53 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import SearchFruits from "./components/Fruits";
-import FormExample from "./components/FormExample";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
-      {/* <SearchFruits /> */}
-      <FormExample />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormExample from "./FormExample";
+const Home = () => {
+  return (
+    <div>
+      {/* <SearchFruits /> */}
+      <FormExample />
+    </div>
+  );
+};
+
+export default Home;
