93c588e4a9cf73d77f30667e0b4de9a5bd8c98d0
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-11-02 21:33:27 +0100

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import FormExample from "./components/FormExample";
+import Fruits from "./components/Fruits";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<Home />} />
+        <Route path="/form-example" element={<FormExample />} />
+        <Route path="/fruits" element={<Fruits />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import FormExample from "./FormExample";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      {/* <SearchFruits /> */}
-      <FormExample />
+    <div className="home-container">
+      <h1>React Examples</h1>
+      <p>Select an example to see more:</p>
+      <div className="link-list">
+        <Link to="/fruits" className="example-link">
+          Search Example
+        </Link>
+        <Link to="/form-example" className="example-link">
+          Form Example
+        </Link>
+      </div>
     </div>
   );
 };
