71eb3b257831ef7c03c068f15b17103d12418348
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2025-01-08 21:40:46 +0100

    search form

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Recipe/Header";
 import "../components/Recipe/Recipe.css";
+import Main from "../components/Recipe/Main";
 
 const Recipe = () => {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 };
